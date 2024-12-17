d56c388f4b67838483b0db91d405abccad0599fc
Kenny <70860732+KennyOliver@users.noreply.github.com>
2024-12-17 22:25:44 +0000

    Feat: Multiple literal/identifier args for `scran` in `.h`
    #9

--- FlavorLang/parser.h
+++ FlavorLang/parser.h
@@ -3,21 +3,59 @@
 
 #include "common.h"
 
+#define MAX_ARGUMENTS 1024
+
 // AST Node Types
 typedef enum
 {
     AST_ASSIGNMENT,
     AST_FUNCTION_CALL,
     AST_PRINT,
-    AST_STRING_LITERAL
+    AST_LITERAL
 } ASTNodeType;
+
+typedef struct
+{
+    enum
+    {
+        LITERAL_STRING,
+        LITERAL_NUMBER
+    } literal_type;
+    union
+    {
+        char *string;
+        double number;
+    } literal_value;
+} LiteralNode;
 
 // AST Node Structure
 typedef struct ASTNode
 {
     ASTNodeType type;
-    char *value;          // for literals, variable names, or function names
-    struct ASTNode *next; // linked list for multiple nodes
+    union
+    {
+        // Assignment
+        struct
+        {
+            char *variable_name;   // variable being assigned
+            struct ASTNode *value; // value being assigned (literal or variable)
+        } assignment;
+
+        // Print statement: list of arguments
+        struct
+        {
+            struct ASTNode **arguments; // array of arguments to print
+            size_t arg_count;
+        } to_print;
+
+        // Literal: a string or number
+        LiteralNode literal;
+
+        // Variable: identifier
+        char *variable_name;
+    };
+
+    struct ASTNode *next; // linked list for multiple statements
 } ASTNode;
 
 // Parser functions
