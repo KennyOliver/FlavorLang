e2cd7c5551fa256f2eafad744cc743ca3ea2a66c
Kenny <70860732+KennyOliver@users.noreply.github.com>
2024-12-17 18:07:02 +0000

    Create parser.h
    #9

---
+++ FlavorLang/parser.h
@@ -0,0 +1,27 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "common.h"
+
+// AST Node Types
+typedef enum
+{
+    AST_VARIABLE_DECLARATION,
+    AST_FUNCTION_CALL,
+    AST_PRINT_STATEMENT,
+    AST_STRING_LITERAL
+} ASTNodeType;
+
+// AST Node Structure
+typedef struct ASTNode
+{
+    ASTNodeType type;
+    char *value;          // for literals, variable names, or function names
+    struct ASTNode *next; // linked list for multiple nodes
+} ASTNode;
+
+// Parser functions
+ASTNode *parse(Token *tokens);
+void free_ast(ASTNode *node);
+
+#endif
