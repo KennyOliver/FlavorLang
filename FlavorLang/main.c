66df5c32adea32962c2af8c622d950f33d2f3cf3
Kenny <70860732+KennyOliver@users.noreply.github.com>
2024-12-20 09:59:47 +0000

    Update main.c
    #27

--- FlavorLang/main.c
+++ FlavorLang/main.c
@@ -1,6 +1,7 @@
 #include "tokenizer/tokenizer.h"
 #include "parser/parser.h"
 #include "interpreter/interpreter.h"
+#include "debug/debug.h"
 
 int main(int argc, char **argv)
 {
@@ -10,12 +11,14 @@ int main(int argc, char **argv)
         fprintf(stderr, "Reason: no `.flv` source file was provided\n");
         return 1;
     }
-    else if (argc > 2)
+    else if (argc > 3)
     {
         fprintf(stderr, "Usage: `%s <file.flv>`\n", argv[0]);
         fprintf(stderr, "Reason: FlavorLang currently only accepts one `.flv` source file\n");
         return 1;
     }
+
+    parse_cli_args(argc, argv);
 
     // Get filename
     const char *filename = argv[1];
