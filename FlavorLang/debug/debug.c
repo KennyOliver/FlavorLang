1ac8b89f110921ca425116a1ea94e5aa5b838b64
Kenny <70860732+KennyOliver@users.noreply.github.com>
2024-12-20 09:29:17 +0000

    Add: `parse_args`
    #27

--- FlavorLang/debug/debug.c
+++ FlavorLang/debug/debug.c
@@ -1,1 +1,16 @@
 #include "debug.h";
+#include <stdio.h>
+#include <string.h>
+
+int debug_flag = 0;
+
+void parse_args(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--debug") == 0)
+        {
+            debug_flag = 1;
+        }
+    }
+}
