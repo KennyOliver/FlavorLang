53f397a3afffc082a83039d6408f6a208c8c3b95
Kenny <70860732+KennyOliver@users.noreply.github.com>
2024-12-28 16:33:10 +0000

    Ran `clang-format`
    #80

--- FlavorLang/parser/parser_state.h
+++ FlavorLang/parser/parser_state.h
@@ -2,15 +2,14 @@
 #define PARSER_STATE_H
 
 #include "../shared/token_types.h"
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-typedef struct
-{
-    Token *tokens;
-    size_t current_token;
-    bool in_switch_block;
-    bool in_function_body;
+typedef struct {
+  Token *tokens;
+  size_t current_token;
+  bool in_switch_block;
+  bool in_function_body;
 } ParserState;
 
 // Create and destroy parser state
@@ -20,7 +19,8 @@ void free_parser_state(ParserState *state);
 // Token navigation
 Token *get_current_token(ParserState *state);
 Token *advance_token(ParserState *state);
-void expect_token(ParserState *state, TokenType type, const char *error_message);
+void expect_token(ParserState *state, TokenType type,
+                  const char *error_message);
 
 // Error handling
 void parser_error(const char *message, Token *token);
--- FlavorLang/shared/token_types.h
+++ FlavorLang/shared/token_types.h
@@ -1,34 +1,32 @@
 #ifndef TOKEN_TYPES_H
 #define TOKEN_TYPES_H
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 // Token types
-typedef enum
-{
-    TOKEN_KEYWORD,
-    TOKEN_IDENTIFIER,
-    TOKEN_FLOAT,
-    TOKEN_INTEGER,
-    TOKEN_STRING,
-    TOKEN_OPERATOR,
-    TOKEN_DELIMITER,
-    TOKEN_FUNCTION_NAME,
-    TOKEN_FUNCTION_PARAMETER,
-    TOKEN_PAREN_OPEN,
-    TOKEN_PAREN_CLOSE,
-    TOKEN_EOF
+typedef enum {
+  TOKEN_KEYWORD,
+  TOKEN_IDENTIFIER,
+  TOKEN_FLOAT,
+  TOKEN_INTEGER,
+  TOKEN_STRING,
+  TOKEN_OPERATOR,
+  TOKEN_DELIMITER,
+  TOKEN_FUNCTION_NAME,
+  TOKEN_FUNCTION_PARAMETER,
+  TOKEN_PAREN_OPEN,
+  TOKEN_PAREN_CLOSE,
+  TOKEN_EOF
 } TokenType;
 
 // Token structure
-typedef struct
-{
-    TokenType type; // type of token
-    char *lexeme;   // the actual text of the token
-    int line;       // line number for error reporting
+typedef struct {
+  TokenType type; // type of token
+  char *lexeme;   // the actual text of the token
+  int line;       // line number for error reporting
 } Token;
 
 #endif
