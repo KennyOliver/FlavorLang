5c462b204e34efd2e06dd34dd6149653a8cd4418
Kenny <70860732+KennyOliver@users.noreply.github.com>
2025-01-01 12:01:39 +0000

    Update keywords.c
    #130

--- src/lexer/keywords.c
+++ src/lexer/keywords.c
@@ -19,7 +19,6 @@ const char *KEYWORDS[] = {
     "rescue",  // catch
     "burn",    // raise error
     "serve",   // output
-    // "sample",  // input
     "plate",   // write file
     "garnish", // append file
     "taste",   // read file
