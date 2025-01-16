ceba42d16c126f752d8ee182c979143c0267aacc
Kenny <70860732+KennyOliver@users.noreply.github.com>
2025-01-16 19:26:48 +0000

    Update test_plugin.c
    #212

--- src/plugins/test_plugin.c
+++ src/plugins/test_plugin.c
@@ -1,16 +1,18 @@
 #include "fl_plugin.h"
 #include <stdio.h>
 
-InterpretResult printSomething(ASTNode *args, Environment *env) {
-    const char *msg = "No message";
-    if (fl_arg_count(args) > 0) {
-        const char *tmp = fl_arg_as_string(args, 0);
-        if (tmp) {
-            msg = tmp;
-        }
+InterpretResult printSomething(struct ASTNode *node, Environment *env) {
+    // Get the arguments list from the function call AST node
+    ASTNode *args = node->function_call.arguments;
+    char *msg = "No message";
+    if (args != NULL && args->type == AST_LITERAL &&
+        args->literal.type == LITERAL_STRING) {
+        msg = args->literal.value.string;
     }
+
     printf("Plugin says: %s\n", msg);
 
+    // Return a default value (0)
     LiteralValue lv = create_default_value();
-    return make_result(lv, 0, 0);
+    return make_result(lv, false, false);
 }
