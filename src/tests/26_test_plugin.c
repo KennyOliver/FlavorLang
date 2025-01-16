8a420b3e2774241604c2f3fea3aa6110e6397641
Kenny <70860732+KennyOliver@users.noreply.github.com>
2025-01-16 17:28:11 +0000

    Fix: `printSomething` returning "No message" regardless of arguments given in `.flv` script
    - Use the node’s `function_call.arguments` field (linked list of argument nodes) to obtain the literal.
    
    #212

--- src/tests/26_test_plugin.c
+++ src/tests/26_test_plugin.c
@@ -5,20 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-InterpretResult printSomething(struct ASTNode *args, Environment *env) {
-    char *msg = NULL;
+// A very simple external function
+InterpretResult printSomething(struct ASTNode *node, Environment *env) {
+    // Get the arguments list from the function call AST node
+    ASTNode *args = node->function_call.arguments;
+    char *msg = "No message";
     if (args != NULL && args->type == AST_LITERAL &&
-        args->literal.type == LITERAL_STRING)
+        args->literal.type == LITERAL_STRING) {
         msg = args->literal.value.string;
-    else
-        msg = "No message";
+    }
 
     printf("Plugin says: %s\n", msg);
 
-    // Return a default value (for example integer 0)
-    LiteralValue lv;
-    lv.type = TYPE_INTEGER;
-    lv.data.integer = 0;
-
+    // Return a default value (0)
+    LiteralValue lv = create_default_value();
     return make_result(lv, false, false);
 }
