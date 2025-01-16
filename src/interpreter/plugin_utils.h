b0f1f76a342b233c89f0fe2dbb366c5bdbcd14ba
Kenny <70860732+KennyOliver@users.noreply.github.com>
2025-01-16 19:34:17 +0000

    Remove: Unused function declarations in `interpreter/plugin_utils.c`/`.h`
    #212

--- src/interpreter/plugin_utils.h
+++ src/interpreter/plugin_utils.h
@@ -7,10 +7,4 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Count the nodes in the linked list of arguments
-int fl_arg_count(ASTNode *args);
-
-// Return the string of the AST literal at index 'index'
-const char *fl_arg_as_string(ASTNode *args, int index);
-
 #endif
